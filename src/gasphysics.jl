79069313e741c3f3a2cb6efd4122fec9d6099f96
Robert Schwarz <mail@rschwarz.net>
2016-06-24 18:33:59 +0200

    gasphysics: add missing operators
     - literal coefficients don't accept whitespace (2x == 2 * x != 2 x)

--- src/gasphysics.jl
+++ src/gasphysics.jl
@@ -1,43 +1,43 @@
 using SIUnits
 
 # adding Bar and Barg (gauge) as units
-const Bar = (1//10^5)Pascal
-const Barg = 1Atmosphere
+const Bar = (1//10^5) * Pascal
+const Barg = Atmosphere
 
 # Source for notation and formulas:
 # "Evaluating Gas Network Capacities", T. Koch et al., SIAM MO21, 2015
 
 # k: roughness, from REKO data (4390 * 0.012mm, 926 * 0.006mm, 166 * 0.002mm)
-const roughness = 0.012*Milli*Meter
+const roughness = 0.012 * Milli*Meter
 
 # D: mean diameter for friction factor
-const diameter_mean = 1.0 Meter
+const diameter_mean = 1.0 * Meter
 
 # λ: using Nikoradse for turbulent flow
 const friction_factor = (log10(diameter_mean/roughness) + 1.138)^(-2)
 
 # R_s: specific gas constant (https://en.wikipedia.org/wiki/Gas_constant)
-const spec_gas_constant = 8.3144598 Joule / Kelvin / Mole
+const spec_gas_constant = 8.3144598 * Joule / Kelvin / Mole
 
 # p_c: default critical pressure in Lamatto++
-const crit_pressure = 46.4512 Bar
+const crit_pressure = 46.4512 * Bar
 
 # T_c: default critical temperature in Lamatto++
-const crit_temperature = 192.033 Kelvin
+const crit_temperature = 192.033 * Kelvin
 
 # z:
 "compressibility factor by AGA8"
 function compressibility(pressure, temperature)
     red_pressure = pressure / crit_pressure
     red_temperature = temperature / crit_temperature
-    z = 1.0 + 0.257 red_pressure - 0.533 red_pressure / red_temperature
+    z = 1.0 + 0.257 * red_pressure - 0.533 * red_pressure / red_temperature
 end
 
 # p_m: TODO find rationale
-const mean_pressure = 60 Bar
+const mean_pressure = 60 * Bar
 
 # T_m: default gas temperature in Lamatto++
-const mean_temperature = 283.15 Kelvin
+const mean_temperature = 283.15 * Kelvin
 
 # z_m: mean compressibility factor
 const mean_compressibility = compressibility(mean_pressure, mean_temperature)
