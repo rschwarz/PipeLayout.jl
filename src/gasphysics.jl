3be601daecec20482ded9c99c9862f95a8f38feb
Robert Schwarz <mail@rschwarz.net>
2016-06-25 21:37:58 +0200

    gasphysics fixes & improvements
     - use *specific* gas constant, instead of universal
     - use shorthand notation for units
     - use shorter names but avoid conflicts

--- src/gasphysics.jl
+++ src/gasphysics.jl
@@ -1,55 +1,61 @@
 using SIUnits
+using SIUnits.ShortUnits
+
+export Bar, Barg, weymouth
 
 # adding Bar and Barg (gauge) as units
-const Bar = (1//10^5) * Pascal
-const Barg = Atmosphere
+const Bar = (1//10^5)Pa
+const Barg = atm
 
 # Source for notation and formulas:
 # "Evaluating Gas Network Capacities", T. Koch et al., SIAM MO21, 2015
 
-# k: roughness, from REKO data (4390 * 0.012mm, 926 * 0.006mm, 166 * 0.002mm)
-const roughness = 0.012 * Milli*Meter
+# pipe roughness, from REKO data (4390 * 0.012mm, 926 * 0.006mm, 166 * 0.002mm)
+const k = 0.012mm
 
-# D: mean diameter for friction factor
-const diameter_mean = 1.0 * Meter
+# pipe (mean) diameter for friction factor
+const D_m = 1.0m
 
-# λ: using Nikoradse for turbulent flow
-const friction_factor = (log10(diameter_mean/roughness) + 1.138)^(-2)
-
-# R_s: specific gas constant (https://en.wikipedia.org/wiki/Gas_constant)
-const spec_gas_constant = 8.3144598 * Joule / Kelvin / Mole
-
-# p_c: default critical pressure in Lamatto++
-const crit_pressure = 46.4512 * Bar
-
-# T_c: default critical temperature in Lamatto++
-const crit_temperature = 192.033 * Kelvin
-
-# z:
-"compressibility factor by AGA8"
-function compressibility(pressure, temperature)
-    red_pressure = pressure / crit_pressure
-    red_temperature = temperature / crit_temperature
-    z = 1.0 + 0.257 * red_pressure - 0.533 * red_pressure / red_temperature
+"friction factor by Nikoradse, for turbulent flow"
+function nikoradse(diameter, roughness)
+    (log10(diameter/roughness) + 1.138)^(-2)
 end
 
-# p_m: TODO find rationale
-const mean_pressure = 60 * Bar
+# friction factor
+const λ = nikoradse(D_m, k)
 
-# T_m: default gas temperature in Lamatto++
-const mean_temperature = 283.15 * Kelvin
+# universal gas constant (https://en.wikipedia.org/wiki/Gas_constant)
+const R = 8.3144598J/K/mol
+
+# molar mass of methane (TODO other mixture?)
+const molmass = 16.04g/mol
+
+# specific gas constant
+const R_s = R/molmass
+
+# critical pressure (default in Lamatto++)
+const p_c = 46.4512Bar
+
+# critical temperature (default in Lamatto++)
+const T_c = 192.033K
+
+# mean pressure (TODO find rationale)
+const p_m = 60Bar
+
+# mean temperature (default in Lamatto++)
+const T_m = 283.15K
+
+"compressibility factor by AGA8"
+function aga8(pressure, temperature)
+    # "reduced" pressure and temperature
+    p_r = pressure / p_c
+    T_r = temperature / T_c
+    z = 1.0 + 0.257 * p_r - 0.533 * p_r/T_r
+end
 
 # z_m: mean compressibility factor
-const mean_compressibility = compressibility(mean_pressure, mean_temperature)
+const z_m = aga8(p_m, T_m)
 
-# Pressure Loss equation ("Weymouth")
-#
-# p_o^2 - p_i^2 = L Λ q|q|
-#
-# or, because we care about length and diameter
-#
-# p_o^2 - p_i^2 = L/D^5 C q|q|
-#
-# with C = (π/4)^(-2) λ R_s z_m T_m
-const C = ((π/4)^(-2) * friction_factor * spec_gas_constant
-           * mean_compressibility * mean_temperature)
+# Pressure Loss equation ("Weymouth"), when we care about length and diameter
+#   p_o^2 - p_i^2 = L/D^5 C q|q|
+const weymouth = (π/4)^(-2) * λ * R_s * z_m * T_m
