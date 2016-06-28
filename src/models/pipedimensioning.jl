6b422bf263e30aa6f472121d83fabee25cf7a0d3
Robert Schwarz <mail@rschwarz.net>
2016-06-28 17:41:15 +0200

    implement and test Kruskal

--- src/models/pipedimensioning.jl
+++ src/models/pipedimensioning.jl
@@ -1,12 +1,13 @@
 # TODO: move this graph code somewhere else!
-using LightGraphs: DiGraph
+using LightGraphs: DiGraph, add_edge!
+using DataStructures: DisjointSets, union!, in_same_set, num_groups
 
 immutable PipeDimInstance
     instance::Instance
     tree::DiGraph
 end
 
-"Compute array of edges (as columns), sorted by length"
+"Compute array of edges (Nodes in columns), sorted by length"
 function sorted_edges(nodes::Vector{Node})
     edges = hcat(combinations(nodes, 2)...)
     sortcols(edges, by=edge -> begin
@@ -16,12 +17,36 @@ function sorted_edges(nodes::Vector{Node})
 end
 
 
-"Compute a minimum spanning tree using Kruskal's algorithm"
+"""Compute a minimum spanning tree using Kruskal's algorithm
+
+Returns a LightGraphs.DiGraphs with integer indices for nodes.
+"""
 function min_spanning_tree(nodes::Vector{Node})
     # TODO, use a Delaunay triangulation instead?
+    n = length(nodes)
+    tree = DiGraph(n)
+
     edges = sorted_edges(nodes)
-    # now, use DisjointSets from DataStructures
-    nothing
+    @assert size(edges, 1) == 2 # column-wise
+
+    nodeidx = [nodes[i] => i for i in 1:n]
+
+    sets = DisjointSets{Node}(nodes)
+    for e in 1:size(edges, 2)
+        u, v = edges[:, e]
+        if !in_same_set(sets, u, v)
+            union!(sets, u, v)
+            add_edge!(tree, nodeidx[u], nodeidx[v])
+        end
+        num_groups(sets) == 1 && break # spanning tree
+    end
+
+    tree
+end
+
+"Build a PipeDimInstance from an Instance, using MST for the topology"
+function instance_from_mst(instance::Instance)
+    PipeDimInstance(instance, min_spanning_tree(instance))
 end
 
 # TODO: actually build the model, think about naming
--- test/models/pipedimensioning.jl
+++ test/models/pipedimensioning.jl
@@ -1,18 +1,32 @@
-import PipeLayout: sorted_edges
+import PipeLayout: sorted_edges, min_spanning_tree
+using PipeLayout
+using LightGraphs
 
-facts("compute sorted edges from nodes") do
+facts("compute MST from nodes") do
     coords = [0 1 0;
               0 0 2]
     nodes = [Node(coords[:,j]...) for j in 1:size(coords, 2)]
     n = length(nodes)
     @fact n --> 3
 
-    edges = sorted_edges(nodes)
-    m = n*(n-1)/2
-    @fact m --> 3
-    @fact size(edges, 1) --> 2
-    @fact size(edges, 2) --> m
-    @fact edges[:, 1] --> Node[nodes[1], nodes[2]]
-    @fact edges[:, 2] --> Node[nodes[1], nodes[3]]
-    @fact edges[:, 3] --> Node[nodes[2], nodes[3]]
+    context("test sorted edges") do
+        edges = sorted_edges(nodes)
+        m = n*(n-1)/2
+        @fact m --> 3
+        @fact size(edges, 1) --> 2
+        @fact size(edges, 2) --> m
+        @fact edges[:, 1] --> Node[nodes[1], nodes[2]]
+        @fact edges[:, 2] --> Node[nodes[1], nodes[3]]
+        @fact edges[:, 3] --> Node[nodes[2], nodes[3]]
+    end
+
+    context("look at resulting MST") do
+        tree = min_spanning_tree(nodes)
+        @fact tree --> is_directed
+        @fact nv(tree) --> 3
+        @fact ne(tree) --> 2
+        @fact tree --> is_connected
+        @fact has_edge(tree, 1, 2) --> true
+        @fact has_edge(tree, 1, 3) --> true
+    end
 end
