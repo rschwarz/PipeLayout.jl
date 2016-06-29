13701746ad912afb3ea664ef7eb749d39a2b6238
Robert Schwarz <mail@rschwarz.net>
2016-06-29 19:12:07 +0200

    wip: pipe dimensioning

--- src/models/pipedimensioning.jl
+++ src/models/pipedimensioning.jl
@@ -1,6 +1,8 @@
 using JuMP
 
 # TODO: actually build the model, think about naming
-function build_pd_model()
+function build_pd_model(inst::Instance, topo::Topology)
+    q = flow(inst, topo)
     m = Model()
+
 end
