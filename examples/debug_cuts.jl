e44fc7326ea5c1ceb8efda828a46dec0c8e76cf0
Robert Schwarz <mail@rschwarz.net>
2016-08-29 18:16:53 +0200

    add file to support debugging

---
+++ examples/debug_cuts.jl
@@ -0,0 +1,22 @@
+using PipeLayout
+import PipeLayout: ploss_coeff_nice
+import PipeLayout.GndStructDiscDiam: run
+using JuMP
+
+nodes = [Node(0,0), Node(45,0), Node(25, 22)]
+demand = 10*[-50, 20, 30]
+bounds = fill(Bounds(60,80), 3)
+diams = [Diameter(t...) for t in [(0.8, 1.0),(1.0, 1.2)]]
+
+topo = Topology([Node(t...) for t in [(0,22), (0,0), (25,22), (25,0), (45,22), (45,0)]],
+                [Arc(t...) for t in [(1,2), (2,1), (3,4), (4,3), (5,6), (6,5),
+                                     (1,3), (3,1), (3,5), (5,3),
+                                     (2,4), (4,2), (4,6), (6,4)]])
+
+inst = Instance(nodes, demand, bounds, diams, ploss_coeff_nice)
+
+result = run(inst, topo, debug=true, addnogoods=false, maxiter=3);
+
+@show result.status
+@show result.dualbound
+@show result.niter
