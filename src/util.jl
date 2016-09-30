ae98c04aefd10aef18ee157ce88ebec37e2a6fd7
Robert Schwarz <mail@rschwarz.net>
2016-09-30 14:21:09 +0200

    add select_subset

--- src/util.jl
+++ src/util.jl
@@ -1,6 +1,16 @@
-export ɛ
+export ɛ, select_subset
 
 # tolerance for numerical comparison
 const ɛ = 1e-6
 
 nonzero(x::Real) = abs(x) > ɛ
+
+" select random subset of a base array of desired size (non-repeating)"
+function select_subset{T}(base::Array{T}, size::Int)
+    @assert size <= length(unique(base))
+    candidates = T[]
+    while length(candidates) < size
+        candidates = unique(append!(candidates, rand(base, size)))
+    end
+    candidates[1:size]
+end
--- test/util.jl
+++ test/util.jl
@@ -7,3 +7,17 @@ facts("numerical comparisons") do
     @fact nonzero(1e-3) --> true
     @fact nonzero(-1e-3) --> true
 end
+
+facts("random subset sampling") do
+    base = collect(1:6)
+    sample0 = select_subset(base, 0)
+    sample3 = select_subset(base, 3)
+    sample6 = select_subset(base, 6)
+
+    @fact length(sample0) --> 0
+    @fact length(sample3) --> 3
+    @fact unique(sample3) --> sample3
+    @fact length(sample6) --> 6
+    @fact unique(sample6) --> sample6
+    @fact_throws select_subset(base, 9)
+end
