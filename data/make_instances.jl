3ef96413eca438427ac970dddd7a0cf6b94523c8
Robert Schwarz <mail@rschwarz.net>
2016-09-30 16:03:02 +0200

    instances: sample demands

--- data/make_instances.jl
+++ data/make_instances.jl
@@ -1,4 +1,5 @@
 using PipeLayout
+import PipeLayout: randdemand
 
 # set random seed for reproducible results
 srand(23)
@@ -14,15 +15,16 @@ for args in [(6, 7, 15.0), (9, 9, 10.0), (13, 13, 7.5), (19, 19, 5.0)]
     ground_structures[key] = squaregrid(args..., antiparallel=true)
 end
 
-
 # for each topology: select subsets of nodes for terminals
 #  [x] randomly
 #  [ ] greedy farthest neighbor
 terminals = Dict{String, Vector{Node}}()
 for (gskey, topo) in ground_structures
-    for nterminals in [3, 5, 7, 11]
-        key = @sprintf "%s-t%02d"  gskey nterminals
-        terminals[key] = select_subset(topo.nodes, nterminals)
+    for nterminals in [3, 5, 7]
+        for variant in ["a", "b"]
+            key = @sprintf "%s-t%02d_%s" gskey nterminals variant
+            terminals[key] = select_subset(topo.nodes, nterminals)
+        end
     end
 end
 
@@ -31,6 +33,21 @@ end
 #  [ ] same, but single source
 #  [ ] hard distribution: maximize transport momentum
 #  [ ] for each normalized demand: several flow scalings
+demands = Dict{String, Vector{Float64}}()
+for (tkey, nodes) in terminals
+    nnodes = length(nodes)
+    nodeidx = collect(1:nnodes)
+
+    # single source, at first position
+    sources, sinks = nodeidx[1:1], nodeidx[2:end]
+    for variant in ["a", "b"]
+        dist = randdemand(nnodes, sources, sinks, 1.0)
+        for flow in [25, 50, 100, 200]
+            key = @sprintf "%s-%02d_%02d_%s_%03d" tkey 1 length(sinks) variant flow
+            demands[key] = flow * dist
+        end
+    end
+end
 
 # uniform pressure bounds
 #  [ ] one for each boundary node
