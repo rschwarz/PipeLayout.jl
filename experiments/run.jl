45b9b9a1931382f423aea3e13e5dda29f0c7a4c3
Robert Schwarz <mail@rschwarz.net>
2016-10-05 12:05:04 +0200

    first draft for runner script

---
+++ experiments/run.jl
@@ -0,0 +1,23 @@
+using PipeLayout
+
+function solve_with(instance::AbstractString, solver::GroundStructureSolver)
+    inst = PipeLayout.read_instance(instance)
+    topo = PipeLayout.read_topology(instance)
+
+    optimize(inst, topo, solver)
+end
+
+config = ARGS[1]
+instname = ARGS[2]
+
+include(config) # creates solver
+
+println("solving $(instname) with $(config)")
+
+tic()
+println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+result = solve_with(instname, solver)
+println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+toc()
+
+println("Status: $(result.status)")
