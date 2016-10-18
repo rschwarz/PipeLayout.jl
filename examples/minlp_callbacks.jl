af30ab2ae8f5d7d265cfb4cadbe90789308c8cfd
Robert Schwarz <mail@rschwarz.net>
2016-10-18 15:59:41 +0200

    add example with callbacks and MINLP model

---
+++ examples/minlp_callbacks.jl
@@ -0,0 +1,48 @@
+using JuMP
+using SCIP
+
+type Problem
+    model
+    x
+    y
+end
+
+function make_model(nonlin=false)::Problem
+    m = Model(solver=SCIPSolver("display/verblevel", 3))
+
+    @variable(m, x[1:9], Bin)
+    @variable(m, -1 ≤ y[1:9] ≤ 1)
+
+    @constraint(m, c1[i=1:9], x[i] <= 3y[i])
+    if nonlin
+        @NLconstraint(m, sum{y[i], i=1:9}^2 == prod{y[i], i=1:9})
+    end
+
+    @objective(m, :Max, sum{y[i] - i*x[i], i=1:9})
+
+    Problem(m, x, y)
+end
+
+function add_lazycons(prob::Problem)
+    # some state for the lazy callback
+    counter = 1
+
+    function lazycb(cb)
+        println("### lazycb is called! ($counter)")
+        @lazyconstraint(cb, prob.x[counter] == prob.y[counter])
+
+        counter = counter % 9 + 1
+    end
+
+    addlazycallback(prob.model, lazycb)
+end
+
+println("<<< First try without nonlinear constraints:")
+prob = make_model(false)
+add_lazycons(prob)
+solve(prob.model)
+
+println("<<< Now try with nonlinear constraints:")
+prob = make_model(true)
+add_lazycons(prob)
+solve(prob.model)
