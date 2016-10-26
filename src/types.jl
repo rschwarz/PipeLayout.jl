29826ef365f188bcccddcd9b24a49bc759de6c99
Robert Schwarz <mail@rschwarz.net>
2016-10-26 16:15:22 +0200

    add type JunctionLocationSolver

--- src/types.jl
+++ src/types.jl
@@ -1,9 +1,12 @@
 # Types for problem classes and solvers
 
-export PipeLayoutSolver, GroundStructureSolver
+export PipeLayoutSolver, GroundStructureSolver, JunctionLocationSolver
 
 "Base type for all solvers"
 abstract PipeLayoutSolver
 
 "Base type for all solvers depending on ground structure"
 abstract GroundStructureSolver <: PipeLayoutSolver
+
+"Base type for all solvers of junction location with fixed topology"
+abstract JunctionLocationSolver <: PipeLayoutSolver
