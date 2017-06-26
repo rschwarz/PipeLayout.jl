cbfb41766c41db2562a7e26a72f5371f5ea8867c
Robert Schwarz <mail@rschwarz.net>
2017-06-26 18:08:01 +0200

    fix deprecated syntax for abstract types

--- src/types.jl
+++ src/types.jl
@@ -4,16 +4,16 @@ export PipeLayoutSolver, GroundStructureSolver, JunctionLocationSolver
 export PipeDimensioningSolver, optimize
 
 "Base type for all solvers"
-abstract PipeLayoutSolver
+abstract type PipeLayoutSolver end
 
 "Base type for all solvers depending on ground structure"
-abstract GroundStructureSolver <: PipeLayoutSolver
+abstract type GroundStructureSolver <: PipeLayoutSolver end
 
 "Base type for all solvers of junction location with fixed topology"
-abstract JunctionLocationSolver <: PipeLayoutSolver
+abstract type JunctionLocationSolver <: PipeLayoutSolver end
 
 "Base type for all solvers of pipe dimensioning with fixed geometry"
-abstract PipeDimensioningSolver <: PipeLayoutSolver
+abstract type PipeDimensioningSolver <: PipeLayoutSolver end
 
 function optimize(inst::Instance, topo::Topology, solver)
     throw(ArgumentError("No method defined for solver type $(typeof(solver))"))
