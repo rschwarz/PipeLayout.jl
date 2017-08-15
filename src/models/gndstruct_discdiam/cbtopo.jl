5d9d81ad3e8b8f51b41db1115120d7a4573b78fb
Robert Schwarz <mail@rschwarz.net>
2017-08-15 22:05:26 +0200

    fix various bugs

--- src/models/gndstruct_discdiam/cbtopo.jl
+++ src/models/gndstruct_discdiam/cbtopo.jl
@@ -12,7 +12,7 @@ struct CallbackTopo <: GroundStructureSolver
     writemodels::Bool
 
     function CallbackTopo(mastersolver, subsolver;
-                          timelimit=Inf, debug=False, writemodels=False)
+                          timelimit=Inf, debug=false, writemodels=false)
         new(mastersolver, subsolver, timelimit, debug, writemodels)
     end
 end
@@ -27,7 +27,7 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
 
     # reuse master problem from IterTopo algorithm
     model, y, q = make_semimaster(inst, topo, solver.mastersolver)
-    writemodels && writeLP(model, "master.lp")
+    solver.writemodels && writeLP(model, "master.lp")
 
     # callback counter and solution store
     counter = 0
@@ -36,7 +36,7 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
     # add callback that solves subproblem
     function cbtopo(cb)
         ysol, qsol = getvalue(y), getvalue(q)
-        debug && println("  cand. sol:$(find(qsol))")
+        solver.debug && println("  cand. sol:$(find(qsol))")
         counter += 1
 
         # TODO: reduce code duplication with itertopo.jl
@@ -47,11 +47,11 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
             cycle = find_cycle(fullcandtopo)
             if length(cycle) == 0
                 # TODO: deal with disconnected candidate?
-                debug && println("  cut off disconnected topology with nogood.")
-                nogood(mastermodel, y, ysol, cb=cb)
+                solver.debug && println("  cut: discon topo (nogood).")
+                nogood(model, y, ysol, cb=cb)
             else
-                debug && println("  cut off non-tree topology, cycle: $(cycle)")
-                avoid_topo_cut(mastermodel, y, topo, cycle, cb=cb)
+                solver.debug && println("  cut: topology w/ cycle: $(cycle)")
+                avoid_topo_cut(model, y, topo, cycle, cb=cb)
             end
             return  # exit callback
         end
@@ -62,7 +62,7 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
         cand = CandSol(zcand, qsol, qsol.^2)
 
         submodel, candarcs, z = make_semisub(inst, topo, cand, solver.subsolver)
-        writemodels && writeLP(submodel, "sub_$(counter).lp")
+        solver.writemodels && writeLP(submodel, "sub_$(counter).lp")
         settimelimit!(submodel, solver.subsolver, finaltime - time())
         substatus = solve(submodel, suppress_warnings=true)
         if substatus == :Optimal
@@ -73,7 +73,7 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
                 znew = fill(false, narcs, ndiams)
                 znew[candarcs, :] = (getvalue(z) .> 0.5)
                 bestsol = CandSol(znew, qsol, qsol.^2)
-                debug && println("  found improving solution: $(primal)")
+                solver.debug && println("  new sol: $(primal)")
             end
         elseif substatus != :Infeasible
             error("Unexpected status: $(:substatus)")
@@ -83,12 +83,12 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
         # TODO: add cut on objective var?
 
         # generate nogood cut and add to master
-        nogood(model, y, ysol, cb)
+        nogood(model, y, ysol, cb=cb)
     end
     addlazycallback(model, cbtopo)
 
     # solve the master (including callback)
-    status = solve(mastermodel, suppress_warnings=true)
+    status = solve(model, suppress_warnings=true)
     if status ≠ :Optimal && status ≠ :Infeasible && status ≠ :UserLimit
         error("Unexpected status: $(status)")
     end
@@ -97,8 +97,8 @@ function PipeLayout.optimize(inst::Instance, topo::Topology,
     if status == :Infeasible && bestsol ≠ nothing
         status = :Optimal
     end
-    debug && println("  problem solved with status $(status).")
+    solver.debug && println("  solved, status: $(status).")
 
     # TODO: extract dual bound, nnodes?
-    Result(status, bestsol, primal, Inf, nothing)
+    Result(status, bestsol, primal, Inf, -1)
 end
