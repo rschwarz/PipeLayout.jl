74425938cbd2ce0ba842c7fa663bff9fce7cbfff
Robert Schwarz <mail@rschwarz.net>
2018-05-18 13:56:59 +0200

    MINLP: set memory limit (to 5GB, by SLURM)

--- experiments/config/gs_dd_minlp.jl
+++ experiments/config/gs_dd_minlp.jl
@@ -1,6 +1,6 @@
 using SCIP
 solver = GndStr.MINLP(
-    SCIPSolver("display/width", 139),
+    SCIPSolver("display/width", 139, "limits/memory", 5000),
     debug=true,
     timelimit=3600.0,
     writemodels=true)
