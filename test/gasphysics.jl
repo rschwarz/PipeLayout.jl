6805628c2c4890f59064101b748e8542ec0a563e
Robert Schwarz <mail@rschwarz.net>
2018-10-23 11:39:20 +0200

    don't use const for local var

--- test/gasphysics.jl
+++ test/gasphysics.jl
@@ -3,11 +3,11 @@ using Unitful
 import PipeLayout: weymouth
 
 @testset "unit consistency in pressure loss" begin
-    const L = 100.0u"km"
-    const D = 1.0u"m"
-    const q = 100.0u"kg/s"
-    const p_i = 80.0u"bar"
-    const C = L/D^5*weymouth
+    L = 100.0u"km"
+    D = 1.0u"m"
+    q = 100.0u"kg/s"
+    p_i = 80.0u"bar"
+    C = L/D^5*weymouth
 
     # pi^2 - po^2 = C*q|q|,  pressure should drop
     @test p_i^2 - C*q*abs.(q) <= p_i^2
