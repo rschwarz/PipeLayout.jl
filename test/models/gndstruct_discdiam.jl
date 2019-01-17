1f79a104d2642ffcdb1187eebd54048c67b6737d
Robert Schwarz <mail@rschwarz.net>
2019-01-17 14:34:06 +0100

    mv test helper function

--- test/models/gndstruct_discdiam.jl
+++ test/models/gndstruct_discdiam.jl
@@ -5,39 +5,6 @@ using JuMP
 using SCIP
 using GLPKMathProgInterface
 
-"Create a grid of square cells with m by n nodes and given edge width."
-function squaregrid(m::Int, n::Int, width::T; antiparallel=false) where T<:Real
-    nodes = Node[]
-    sizehint!(nodes, m*n)
-    for j in 1:n
-        x = width * (j - 1)
-        for i in 1:m
-            push!(nodes, Node(x, width * (m - i)))
-        end
-    end
-    nodeidx = reshape(1:m*n, (m,n))
-
-    arcs = Arc[]
-    narcs = (m-1)*n + m*(n-1)
-    sizehint!(arcs, antiparallel ? 2*narcs : narcs)
-    # columns top-down
-    for j in 1:n
-        for i in 1:m-1
-            push!(arcs, Arc(nodeidx[i,j], nodeidx[i+1,j]))
-            antiparallel && push!(arcs, Arc(nodeidx[i+1,j], nodeidx[i,j]))
-        end
-    end
-    # rows left-right
-    for i in 1:m
-        for j in 1:n-1
-            push!(arcs, Arc(nodeidx[i,j], nodeidx[i,j+1]))
-            antiparallel && push!(arcs, Arc(nodeidx[i,j+1], nodeidx[i,j]))
-        end
-    end
-
-    Topology(nodes, arcs)
-end
-
 include("gndstruct_discdiam_iter.jl")
 include("gndstruct_discdiam_minlp.jl")
 include("gndstruct_discdiam_cb.jl")
