5cefd6cf2c1afbacffa2c753d3c920cb8bb7e14e
Robert Schwarz <mail@rschwarz.net>
2020-03-14 16:16:10 +0100

    add kwarg "markersize" for draw!

--- src/viz.jl
+++ src/viz.jl
@@ -4,12 +4,12 @@ function empty_scene(; kwargs...)
     return Scene(; kwargs..., show_axis=false, scale_plot=false)
 end
 
-function draw!(scene, topo::Topology)
+function draw!(scene, topo::Topology; markersize=2)
     # arcs (bottom layer)
     positions = Array(topo.nodes[vcat(collect.(topo.arcs)...)])
     linesegments!(scene, positions)
 
     # nodes (on top)
     positions = topo.nodes
-    scatter!(scene, positions, markersize=4)
+    scatter!(scene, positions, markersize=markersize)
 end
