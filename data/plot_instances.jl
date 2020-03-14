28689c84ecbea3b289c0fbc72d313d12f33c24be
Robert Schwarz <mail@rschwarz.net>
2020-03-14 16:17:09 +0100

    fix plot_instances.jl

--- data/plot_instances.jl
+++ data/plot_instances.jl
@@ -1,16 +1,24 @@
+using AbstractPlotting
 using CairoMakie
 using PipeLayout
 
-open(ARGS[1]) do f
-    for line in eachline(f)
-        key = strip(line)
-        println(key)
-        inst = PipeLayout.read_instance(".", key)
-        topo = PipeLayout.read_topology(".", key)
+function draw_instance(key)
+    inst = PipeLayout.read_instance(".", key)
+    topo = PipeLayout.read_topology(".", key)
 
-        scene = PipeLayout.empty_scene()
-        draw!(scene, topo)
-        # TODO: draw node demand
-        save("$key.png", scene)
+    scene = PipeLayout.empty_scene()
+    PipeLayout.draw!(scene, topo)
+    # TODO: draw node demand
+    save("$key.png", scene)
+end
+
+function draw_instances(filename)
+    open(filename) do f
+        for line in eachline(f)
+            key = strip(line)
+            draw_instance(key)
+        end
     end
 end
+
+draw_instances(ARGS[1])
